cfc19e4b509ea356db7ec33a4ccc29d71eef9791
SaiKrishna <saikrishnapuppireddy@gmail.com>
2024-04-26 08:52:22 +0530

    added sql file

---
+++ TrainReservationSystem/src/main/resources/database_sql/trains.sql
@@ -0,0 +1,193 @@
+-- 12760 - Hyderabad to Chennai
+insert into train values(12760, "Charminar Express", "Hyderabad", "Chennai", 455, 2825, 1685, 1190);
+
+insert into train_availability values(12760, str_to_date("12-03-2024","%d-%m-%Y"), 120, 60, 80, 100);
+insert into train_availability values(12760, str_to_date("13-03-2024","%d-%m-%Y"), 120, 60, 80, 100);
+insert into train_availability values(12760, str_to_date("14-03-2024","%d-%m-%Y"), 120, 60, 80, 100);
+insert into train_availability values(12760, str_to_date("15-03-2024","%d-%m-%Y"), 120, 60, 80, 100);
+insert into train_availability values(12760, str_to_date("16-03-2024","%d-%m-%Y"), 15, 20, 8, 9);
+insert into train_availability values(12760, str_to_date("17-03-2024","%d-%m-%Y"), 120, 60, 80, 100);
+insert into train_availability values(12760, str_to_date("18-03-2024","%d-%m-%Y"), 120, 60, 80, 100);
+insert into train_availability values(12760, str_to_date("19-03-2024","%d-%m-%Y"), 120, 30, 80, 55);
+insert into train_availability values(12760, str_to_date("20-03-2024","%d-%m-%Y"), 120, 60, 80, 100);
+
+-- 12604 - Hyderabad to Chennai
+insert into train values(12604, "Chennai SF EXP", "Hyderabad", "Chennai", null, 2595, 1550, 1100);
+
+insert into train_availability values(12604, str_to_date("12-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(12604, str_to_date("13-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(12604, str_to_date("14-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(12604, str_to_date("15-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(12604, str_to_date("16-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(12604, str_to_date("17-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(12604, str_to_date("18-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(12604, str_to_date("19-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(12604, str_to_date("20-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+
+-- 17652 - Hyderabad to Chennai
+insert into train values(17652, "KCG CGL Express", "Hyderabad", "Chennai", 415, null, 1610, 1125);
+
+insert into train_availability values(17652, str_to_date("12-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+insert into train_availability values(17652, str_to_date("13-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+insert into train_availability values(17652, str_to_date("14-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+insert into train_availability values(17652, str_to_date("15-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+insert into train_availability values(17652, str_to_date("16-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+insert into train_availability values(17652, str_to_date("17-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+insert into train_availability values(17652, str_to_date("18-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+insert into train_availability values(17652, str_to_date("19-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+insert into train_availability values(17652, str_to_date("20-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+
+-- ---------------------------------------------------------------------------------------------------------
+
+-- 12761 - chennai to hyderabad
+insert into train values(12761, "Charminar Express", "Chennai", "Hyderabad", 455, 2825, 1685, 1190);
+
+insert into train_availability values(12761, str_to_date("12-03-2024","%d-%m-%Y"), 120, 60, 80, 100);
+insert into train_availability values(12761, str_to_date("13-03-2024","%d-%m-%Y"), 120, 60, 80, 100);
+insert into train_availability values(12761, str_to_date("14-03-2024","%d-%m-%Y"), 120, 60, 80, 100);
+insert into train_availability values(12761, str_to_date("15-03-2024","%d-%m-%Y"), 120, 60, 80, 100);
+insert into train_availability values(12761, str_to_date("16-03-2024","%d-%m-%Y"), 82, 30, 38, 22);
+insert into train_availability values(12761, str_to_date("17-03-2024","%d-%m-%Y"), 120, 60, 80, 100);
+insert into train_availability values(12761, str_to_date("18-03-2024","%d-%m-%Y"), 120, 33, 80, 100);
+insert into train_availability values(12761, str_to_date("19-03-2024","%d-%m-%Y"), 120, 30, 50, 55);
+insert into train_availability values(12761, str_to_date("20-03-2024","%d-%m-%Y"), 120, 60, 63, 100);
+
+-- 12603 - chennai to hyderabad
+insert into train values(12603, "Chennai SF EXP", "Chennai", "Hyderabad", null, 2595, 1550, 1100);
+
+insert into train_availability values(12603, str_to_date("12-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(12603, str_to_date("13-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(12603, str_to_date("14-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(12603, str_to_date("15-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(12603, str_to_date("16-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(12603, str_to_date("17-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(12603, str_to_date("18-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(12603, str_to_date("19-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(12603, str_to_date("20-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+
+-- 17653 - chennai to hyderabad
+insert into train values(17653, "KCG CGL Express", "Chennai", "Hyderabad", 415, null, 1610, 1125);
+
+insert into train_availability values(17653, str_to_date("12-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+insert into train_availability values(17653, str_to_date("13-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+insert into train_availability values(17653, str_to_date("14-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+insert into train_availability values(17653, str_to_date("15-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+insert into train_availability values(17653, str_to_date("16-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+insert into train_availability values(17653, str_to_date("17-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+insert into train_availability values(17653, str_to_date("18-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+insert into train_availability values(17653, str_to_date("19-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+insert into train_availability values(17653, str_to_date("20-03-2024","%d-%m-%Y"), 100, -1, 60, 90);
+
+-- ------------------------------------------------------------------------------------------------------
+
+-- 20703 - Hyderabad to Bangalore
+insert into train values(20703, "YPR VandeBharat", "Hyderabad", "Bangalore", null, 1600, 2915, null);
+
+insert into train_availability values(20703, str_to_date("12-03-2024","%d-%m-%Y"), -1, 60, 80, -1);
+insert into train_availability values(20703, str_to_date("13-03-2024","%d-%m-%Y"), -1, 60, 55, -1);
+insert into train_availability values(20703, str_to_date("14-03-2024","%d-%m-%Y"), -1, 22, 80, -1);
+insert into train_availability values(20703, str_to_date("15-03-2024","%d-%m-%Y"), -1, 60, 80, -1);
+insert into train_availability values(20703, str_to_date("16-03-2024","%d-%m-%Y"), -1, 20, 8, -1);
+insert into train_availability values(20703, str_to_date("17-03-2024","%d-%m-%Y"), -1, 60, 80, -1);
+insert into train_availability values(20703, str_to_date("18-03-2024","%d-%m-%Y"), -1, 60, 80, -1);
+insert into train_availability values(20703, str_to_date("19-03-2024","%d-%m-%Y"), -1, 30, 80, -1);
+insert into train_availability values(20703, str_to_date("20-03-2024","%d-%m-%Y"), -1, 60, 80, -1);
+
+-- 22692 - Hyderabad to Bangalore
+insert into train values(22692, "SBC Rajdhani", "Hyderabad", "Bangalore", null, 3155, 2565, 1885);
+
+insert into train_availability values(22692, str_to_date("12-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(22692, str_to_date("13-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(22692, str_to_date("14-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(22692, str_to_date("15-03-2024","%d-%m-%Y"), -1, 32, 5, 23);
+insert into train_availability values(22692, str_to_date("16-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(22692, str_to_date("17-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(22692, str_to_date("18-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(22692, str_to_date("19-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(22692, str_to_date("20-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+
+-- -------------------------------------------------------------------------------------------------------
+-- 20704 - Bangalore to Hyderabad
+insert into train values(20704, "YPR VandeBharat", "Bangalore", "Hyderabad", null, 1600, 2915, null);
+
+insert into train_availability values(20704, str_to_date("12-03-2024","%d-%m-%Y"), -1, 60, 80, -1);
+insert into train_availability values(20704, str_to_date("13-03-2024","%d-%m-%Y"), -1, 60, 55, -1);
+insert into train_availability values(20704, str_to_date("14-03-2024","%d-%m-%Y"), -1, 22, 80, -1);
+insert into train_availability values(20704, str_to_date("15-03-2024","%d-%m-%Y"), -1, 60, 80, -1);
+insert into train_availability values(20704, str_to_date("16-03-2024","%d-%m-%Y"), -1, 20, 8, -1);
+insert into train_availability values(20704, str_to_date("17-03-2024","%d-%m-%Y"), -1, 60, 80, -1);
+insert into train_availability values(20704, str_to_date("18-03-2024","%d-%m-%Y"), -1, 60, 80, -1);
+insert into train_availability values(20704, str_to_date("19-03-2024","%d-%m-%Y"), -1, 30, 80, -1);
+insert into train_availability values(20704, str_to_date("20-03-2024","%d-%m-%Y"), -1, 60, 80, -1);
+
+-- 22693 -  Bangalore to Hyderabad
+insert into train values(22693, "SBC Rajdhani", "Bangalore", "Hyderabad", null, 3155, 2565, 1885);
+
+insert into train_availability values(22693, str_to_date("12-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(22693, str_to_date("13-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(22693, str_to_date("14-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(22693, str_to_date("15-03-2024","%d-%m-%Y"), -1, 32, 5, 23);
+insert into train_availability values(22693, str_to_date("16-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(22693, str_to_date("17-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(22693, str_to_date("18-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(22693, str_to_date("19-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+insert into train_availability values(22693, str_to_date("20-03-2024","%d-%m-%Y"), -1, 60, 80, 100);
+
+-- --------------------------------------------------------------------------------------------------
+--  12007  - Chennai to Bangalore
+insert into train values(12007, "MYS Shatabdi", "Chennai", "Bangalore", null, null, 1525, 980);
+
+insert into train_availability values(12007, str_to_date("12-03-2024","%d-%m-%Y"), -1, -1, 100, 120);
+insert into train_availability values(12007, str_to_date("13-03-2024","%d-%m-%Y"), -1, -1, 10, 20);
+insert into train_availability values(12007, str_to_date("14-03-2024","%d-%m-%Y"), -1, -1, 10, 40);
+insert into train_availability values(12007, str_to_date("15-03-2024","%d-%m-%Y"), -1, -1, 100, 120);
+insert into train_availability values(12007, str_to_date("16-03-2024","%d-%m-%Y"), -1, -1, 100, 120);
+insert into train_availability values(12007, str_to_date("17-03-2024","%d-%m-%Y"), -1, -1, 100, 120);
+insert into train_availability values(12007, str_to_date("18-03-2024","%d-%m-%Y"), -1, -1, 100, 120);
+insert into train_availability values(12007, str_to_date("19-03-2024","%d-%m-%Y"), -1, -1, 100, 120);
+insert into train_availability values(12007, str_to_date("20-03-2024","%d-%m-%Y"), -1, -1, 100, 120);
+
+--  12639  - Chennai to Bangalore
+insert into train values(12639, "Brindavan SF EXP", "Chennai", "Bangalore", 600, 1950, 1525, 980);
+
+insert into train_availability values(12639, str_to_date("12-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+insert into train_availability values(12639, str_to_date("13-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+insert into train_availability values(12639, str_to_date("14-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+insert into train_availability values(12639, str_to_date("15-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+insert into train_availability values(12639, str_to_date("16-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+insert into train_availability values(12639, str_to_date("17-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+insert into train_availability values(12639, str_to_date("18-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+insert into train_availability values(12639, str_to_date("19-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+insert into train_availability values(12639, str_to_date("20-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+-- ----------------------------------------------------------------------------------------------------
+--  12008  -  Bangalore to Chennai 
+insert into train values(12008, "MYS Shatabdi", "Bangalore", "Chennai", null, null, 1682, 993);
+
+insert into train_availability values(12008, str_to_date("12-03-2024","%d-%m-%Y"), -1, -1, 100, 120);
+insert into train_availability values(12008, str_to_date("13-03-2024","%d-%m-%Y"), -1, -1, 10, 20);
+insert into train_availability values(12008, str_to_date("14-03-2024","%d-%m-%Y"), -1, -1, 10, 40);
+insert into train_availability values(12008, str_to_date("15-03-2024","%d-%m-%Y"), -1, -1, 100, 120);
+insert into train_availability values(12008, str_to_date("16-03-2024","%d-%m-%Y"), -1, -1, 100, 120);
+insert into train_availability values(12008, str_to_date("17-03-2024","%d-%m-%Y"), -1, -1, 100, 120);
+insert into train_availability values(12008, str_to_date("18-03-2024","%d-%m-%Y"), -1, -1, 100, 120);
+insert into train_availability values(12008, str_to_date("19-03-2024","%d-%m-%Y"), -1, -1, 100, 120);
+insert into train_availability values(12008, str_to_date("20-03-2024","%d-%m-%Y"), -1, -1, 100, 120);
+
+
+--  12638  - Bangalore to Chennai
+insert into train values(12638, "Brindavan SF EXP", "Bangalore", "Chennai", 600, 1950, 1525, 980);
+
+insert into train_availability values(12638, str_to_date("12-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+insert into train_availability values(12638, str_to_date("13-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+insert into train_availability values(12638, str_to_date("14-03-2024","%d-%m-%Y"), 5, 4, 6, 2);
+insert into train_availability values(12638, str_to_date("15-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+insert into train_availability values(12638, str_to_date("16-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+insert into train_availability values(12638, str_to_date("17-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+insert into train_availability values(12638, str_to_date("18-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+insert into train_availability values(12638, str_to_date("19-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+insert into train_availability values(12638, str_to_date("20-03-2024","%d-%m-%Y"), 90, 80, 100, 120);
+
+
+
+UPDATE train_availability SET DOJ = DATE_ADD(DOJ, INTERVAL 10 DAY);
+
